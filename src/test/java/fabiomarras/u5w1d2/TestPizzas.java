7312409d67485a4e0c4c0982164593b2b3335861
FabioMarras <fabio.marras@yahoo.com>
2023-11-02 15:44:33 +0100

    add some test

--- src/test/java/fabiomarras/u5w1d2/TestPizzas.java
+++ src/test/java/fabiomarras/u5w1d2/TestPizzas.java
@@ -2,12 +2,18 @@ package fabiomarras.u5w1d2;
 
 import fabiomarras.u5w1d2.entities.Drinks;
 import fabiomarras.u5w1d2.entities.Pizzas;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -15,14 +21,19 @@ import static org.junit.jupiter.api.Assertions.*;
 public class TestPizzas {
 
     @Autowired
-    private Pizzas pizza;
+    private Pizzas PizzaIMargherita;
 
+    @Autowired
+    private Pizzas PizzaIMargheritaXL;
 
     @Autowired
     private Drinks water;
 
     @Autowired
     private Drinks lemonade;
+
+    @Autowired
+    private List<Drinks> allDrinks;
 
     @BeforeAll
     static void beforeAll(){
@@ -34,7 +45,7 @@ public class TestPizzas {
         System.out.println("TestPrice");
 
         //assertNotNull(pizza);
-        assertTrue(pizza.getPrice() > 0.00);
+        assertTrue(PizzaIMargherita.getPrice() > 0.00);
     }
 
     @Test
@@ -47,5 +58,23 @@ public class TestPizzas {
     public void lemondeIsPresence(){
         System.out.println("lemondeIsPresence");
         assertEquals("lemonade", lemonade.getName());
+    }
+
+    @Test
+    public void notSamePizza(){
+        System.out.println("notSamePizza");
+        assertNotEquals("PizzaIMargherita", "PizzaIMargheritaXL");
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"L", "l", "p"})
+    void findByLetterL(String letterL) {
+        List<Drinks> allDrink = allDrinks.stream().filter(drinks -> drinks.getName().startsWith(letterL)).toList();
+                assertEquals(allDrink.size(),1);
+    }
+
+    @AfterAll
+    static void AfterAll(){
+        System.out.println("After ALL, FINISH");
     }
 }
